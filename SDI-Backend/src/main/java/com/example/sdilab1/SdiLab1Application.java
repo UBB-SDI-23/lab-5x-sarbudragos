1a56e0a425cdf3502fa771cd80e52999257466a1
sarbudragos <dragossarbu2003@gmail.com>
2023-04-26 22:44:45 +0300

    tomcat server

--- SDI-Backend/src/main/java/com/example/sdilab1/SdiLab1Application.java
+++ SDI-Backend/src/main/java/com/example/sdilab1/SdiLab1Application.java
@@ -2,10 +2,16 @@ package com.example.sdilab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SdiLab1Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SdiLab1Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SdiLab1Application.class, args);
